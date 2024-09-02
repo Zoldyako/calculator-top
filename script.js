4e1e50fcc5beb73d4bf6b7a27d98c52e8b94084e
José Victor <zoldyakopersonal@gmail.com>
2024-09-02 15:04:24 -0300

    Using defer to wait for the page to load

--- script.js
+++ script.js
@@ -1,80 +1,59 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let a, b, result = [];
-    let operators = ["+", "-", "*", "%"];
-    let numbers = [ 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ];
+let a, b, result = [];
+let operators = ["+", "-", "*", "%"];
+let numbers = [ 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ];
 
-    const BTN_OPERATORS = document.querySelectorAll('.btn-operator');
-    const BTN_NUMBERS   = document.querySelectorAll('.btn-number');
-    const BTN_EQUAL     = document.querySelector('.btn-equal');
-    const BTN_CLEAR     = document.querySelector('.btn-clear');
-    const HISTORY       = document.querySelector('#history');
-    const CURRENT       = document.querySelector('#current-calc');
+const BTN_OPERATORS = document.querySelectorAll('.btn-operator');
+const BTN_NUMBERS   = document.querySelectorAll('.btn-number');
+const BTN_EQUAL     = document.querySelector('.btn-equal');
+const BTN_CLEAR     = document.querySelector('.btn-clear');
+const HISTORY       = document.querySelector('#history');
+const CURRENT       = document.querySelector('#current-calc');
 
 
-    function operate(a, b, operator) {
-        if (operator === '+') { return alert(a + b); }
-        else if (operator === '-') { return alert(a - b); }
-        else if (operator === '*') { return alert(a * b); }
-        else if (operator === '/') { return alert(a / b); }
+function operate(a, b, operator) {
+    if (operator === '+') { return alert(a + b); }
+    else if (operator === '-') { return alert(a - b); }
+    else if (operator === '*') { return alert(a * b); }
+    else if (operator === '/') { return alert(a / b); }
+}
+
+
+function populateDisplay(CURRENT) {
+    let history = document.createElement('p');
+    history.innerText = CURRENT.innerText;
+
+    HISTORY.appendChild(history);
+}
+
+
+function addToCurrentCalc(text) {
+    if (CURRENT.innerText.length >= 20) {
+        return alert("Máximo de 20 dígitos");
     }
 
-
-    function populateDisplay(CURRENT) {
-        let history = document.createElement('p');
-        history.innerText = CURRENT.innerText;
-
-        HISTORY.appendChild(history);
-    }
+    CURRENT.innerText += text;
+}
 
 
-    function addToCurrentCalc(text) {
-        if (CURRENT.innerText.length >= 20) {
-            return alert("Máximo de 20 dígitos");
-        }
-
-        CURRENT.innerText += text;
-    }
-
-    
-    BTN_NUMBERS.forEach((button, index) => {
-        button.addEventListener('click', () => {
-            addToCurrentCalc(numbers[index]);
-        });
+BTN_NUMBERS.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        addToCurrentCalc(numbers[index]);
     });
+});
 
 
-    BTN_CLEAR.addEventListener('click', () => { 
-        CURRENT.innerText = '';
+BTN_CLEAR.addEventListener('click', () => { 
+    CURRENT.innerText = '';
+});
+
+
+BTN_OPERATORS.forEach((button, index) => {
+    button.addEventListener('click', () => {
+        addToCurrentCalc(operators[index]);
     });
+});
 
 
-    BTN_OPERATORS.forEach((button, index) => {
-        button.addEventListener('click', () => {
-            addToCurrentCalc(operators[index]);
-        });
-    });
-
-
-    BTN_EQUAL.addEventListener('click', () => {
-        populateDisplay(CURRENT)
-    });
-
-
-
-
-
-    
-    // btn_equal.addEventListener('click', () => {
-        
-    //     if (a === undefined || b === undefined) {
-    //         a = undefined, b = undefined;
-    //         return alert('ERROR: You need 2 values for the calculation!');
-    //     }
-
-    //     else if (operator === undefined) {
-    //         return alert("ERROR: You need to select an operator for the calculation!");
-    //     }
-
-    //     operate(a, b, operator);
-    // });
+BTN_EQUAL.addEventListener('click', () => {
+    populateDisplay(CURRENT);
 });
