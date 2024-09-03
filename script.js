3b07b765161ce3dc20920aa22ad309cfc0ef2916
José Victor <zoldyakopersonal@gmail.com>
2024-09-03 01:17:56 -0300

    Calculator properly working

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-let a, b, result = [];
-let operators = ["+", "-", "*", "%"];
+let currentNumber = 0, previousNumber = 0, operator, result = [];
+let operators = ["+", "-", "*", "/"];
 let numbers = [ 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 ];
+let isOperatorClicked = false;
+
 
 const BTN_OPERATORS = document.querySelectorAll('.btn-operator');
 const BTN_NUMBERS   = document.querySelectorAll('.btn-number');
@@ -11,17 +13,47 @@ const CURRENT       = document.querySelector('#current-calc');
 
 
 function operate(a, b, operator) {
-    if (operator === '+') { return alert(a + b); }
-    else if (operator === '-') { return alert(a - b); }
-    else if (operator === '*') { return alert(a * b); }
-    else if (operator === '/') { return alert(a / b); }
+    let computation;
+    
+    switch (operator) {
+        case '+':
+            computation = a + b;
+            break;
+        case '-':
+            computation = a - b;
+            break;
+        case '*':
+            computation = a * b;
+            break;
+        case '/':
+            computation = a / b;
+            break;
+    }
+
+
+    if (computation == undefined) {
+        CURRENT.innerText = ''
+        currentNumber = 0;
+        previousNumber = 0;
+        alert("You didn't select any operation")
+    }
+
+    else if (computation.toString().split('.')[1]?.length > 1) {
+        populateDisplay(computation.toFixe(1));
+    }
+    
+    else {
+        populateDisplay(computation);
+    }
+    
+    currentNumber = computation;
+    operator = '';
 }
 
 
-function populateDisplay(CURRENT) {
+function populateDisplay(number) {
     let history = document.createElement('p');
-    history.innerText = CURRENT.innerText;
-
+    history.innerText = number;
     HISTORY.appendChild(history);
 }
 
@@ -32,28 +64,58 @@ function addToCurrentCalc(text) {
     }
 
     CURRENT.innerText += text;
+    
 }
+
 
 
 BTN_NUMBERS.forEach((button, index) => {
     button.addEventListener('click', () => {
+        if (isOperatorClicked) {
+            CURRENT.innerText = '';
+            isOperatorClicked = false;
+        }
         addToCurrentCalc(numbers[index]);
+        currentNumber = parseFloat(CURRENT.innerText);
     });
 });
 
 
 BTN_CLEAR.addEventListener('click', () => { 
     CURRENT.innerText = '';
+    currentNumber = 0; 
+    previousNumber = 0;
 });
 
 
 BTN_OPERATORS.forEach((button, index) => {
     button.addEventListener('click', () => {
-        addToCurrentCalc(operators[index]);
+        if (!isOperatorClicked) {
+            previousNumber = currentNumber;
+        }
+
+        operator = operators[index];
+        addToCurrentCalc(operator);
+        isOperatorClicked = true;
     });
 });
 
 
 BTN_EQUAL.addEventListener('click', () => {
-    populateDisplay(CURRENT);
+
+    if (previousNumber == 0 &&  currentNumber == 0 && operator == '/') {
+        alert("You can't divide nothing for nothing dummy :P");
+        return
+    }
+
+    else if (previousNumber == 0 && currentNumber == 0) {
+        alert("Please select a number");
+        return;
+    }
+
+    else {
+        operate(previousNumber, currentNumber, operator);
+        isOperatorClicked = false;
+        CURRENT.innerText = '';
+    }    
 });
